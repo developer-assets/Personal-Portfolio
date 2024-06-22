e88f96aab41ea0b90b2c6d9a511873244e533f37
developer-assets <999patrickobama@gmail.com>
2024-06-22 15:37:07 +0300

    Fix prerender bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Image from 'next/image';
 
 const Home: React.FC = () => {
-	AOS.init();
+	useEffect(() => {
+		AOS.init();
+	}, []);
 
 	return (
 		<main>
